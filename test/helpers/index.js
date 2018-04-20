496053f14e9b3385505311a0e630044d076c8051
Terkel Gjervig <terkel@terkel.com>
2018-04-19 21:20:07 -0700

    fix win util

--- test/helpers/index.js
+++ test/helpers/index.js
@@ -1,9 +1,8 @@
 const isWin = process.platform === 'win32';
 
 // unixify path for cross-platform testing
-function unixify(str) {
-  console.log(str);
-  return isWin ? str.replace(/\\/g, '/') : str;
+function unixify(arr) {
+  return isWin ? arr.map(str => str.replace(/\\/g, '/')) : arr;
 }
 
 function toIgnore(str) {
